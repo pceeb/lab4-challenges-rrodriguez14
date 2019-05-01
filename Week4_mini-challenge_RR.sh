2915789d86784bc13ba2dff191a1736dbc71a30e
rrodriguez14 <49268353+rrodriguez14@users.noreply.github.com>
2019-04-30 17:57:58 -0700

    Update Week4_mini-challenge_RR.sh

--- Week4_mini-challenge_RR.sh
+++ Week4_mini-challenge_RR.sh
@@ -1,1 +1,12 @@
+1. echo "Cats eat 5 billion birds a year" | sed 's/\(Cats eat\) \(5 billion\) \(birds a year\)/\2\? \2\!/'
+5 billion? 5 billion!
 
+
+2.echo "12345abcde678910fghij" | sed 's/\(12345\)\(abcde\)\(678910\)\(fghij\)/\2\4\1\3/'
+abcdefghij12345678910
+
+3.echo "12345abcde678910fghij" | sed 's/\(12345\)\(abcde\)\(678910\)\(fghij\)/\2\4\1\3/'| sed 's/../&\t/g'
+
+ab	cd	ef	gh	ij	12	34	56	78	91	0
+
+4.echo "12345abcde678910fghij" |sed 's/\(12345\)\(abcde\)\(678910\)\(fghij\)/\2\4/' | sed 's/\(ab\)\(cd\)\(ef\)\(gh\)\(ij\)/\1\t\2\t\3\t\4\t\5\t/'
